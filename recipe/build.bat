8c434c6be1d8f0ec9677010531f978bc88ba1a2b
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-26 14:07:27 -0800

    use rattler

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,8 +1,11 @@
+@echo on
+@setlocal EnableDelayedExpansion
+
 cd cli\go-git || goto :error
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
+go build -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
 go-licenses save . --save_path=%SRC_DIR%\license-files --ignore=github.com/go-git || goto :error
 
-goto :EOF
+goto :eof
 
 :error
 echo Failed with error #%errorlevel%.
